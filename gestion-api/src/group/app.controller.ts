81e25fc6427a6b6efc8e131dc83372f9897dff52
IGSparkew <40170910+IGSparkew@users.noreply.github.com>
2023-07-22 21:17:38 +0200

    feat: check auth in group grpc

--- gestion-api/src/group/app.controller.ts
+++ gestion-api/src/group/app.controller.ts
@@ -1,37 +1,78 @@
-import { Controller } from "@nestjs/common";
-import { GroupServiceControllerMethods, GroupServiceController, CreateGroupRequest, CreateGroupResponse, GetGroupRequest, GetGroupResponse, Empty, GetAllGroupsResponse } from "../stubs/group/v1alpha/group";
-import { Metadata } from "@grpc/grpc-js";
-import { GroupService } from "./app.service";
-import { RpcException } from "@nestjs/microservices";
-import { group } from "console";
-import { Observable } from "rxjs";
+import { Controller } from '@nestjs/common';
+import {
+  GroupServiceControllerMethods,
+  GroupServiceController,
+  CreateGroupRequest,
+  CreateGroupResponse,
+  GetGroupRequest,
+  GetGroupResponse,
+  Empty,
+  GetAllGroupsResponse,
+} from '../stubs/group/v1alpha/group';
+import { Metadata } from '@grpc/grpc-js';
+import { GroupService } from './app.service';
+import { RpcException } from '@nestjs/microservices';
+import { group } from 'console';
+import { Observable } from 'rxjs';
+import { AuthCheckService } from 'src/check_auth/app.service';
 
 @Controller()
 @GroupServiceControllerMethods()
 export class GroupController implements GroupServiceController {
+  constructor(
+    private readonly groupService: GroupService,
+    private readonly checkAuthService: AuthCheckService,
+  ) {}
 
-    constructor(private readonly groupService:GroupService) {}
+  async findAllGroup(
+    request: Empty,
+    metadata?: Metadata,
+  ): Promise<GetAllGroupsResponse> {
+    const right_auth = await this.checkAuthService.checkTokenApi(metadata);
 
-
-    findAllGroup(request: Empty, metadata?: Metadata): Promise<GetAllGroupsResponse> {
-        return this.groupService.findAll()
+    if (!right_auth) {
+      throw new RpcException('Error unauthorized auth!');
     }
 
-    async createGroup(request: CreateGroupRequest,metadata?: Metadata): Promise<CreateGroupResponse> {
-        if (!request || !request.name ||request.name == "") {
-            throw new RpcException("Error on input name");
-        }
-        return await this.groupService.create(request.name);
-      }
-    
-    async findGroup(request: GetGroupRequest,metadata?: Metadata): Promise<GetGroupResponse>{
-        if(!request || !request.groupId) {
-            throw new RpcException("Error on input group_id");
-        }
-        const groupFounded = await this.groupService.get(request.groupId);
-        if (groupFounded == undefined) {
-            throw new RpcException("Error no group found");
-        }
-        return groupFounded;
-      }
+    return this.groupService.findAll();
+  }
+
+  async createGroup(
+    request: CreateGroupRequest,
+    metadata?: Metadata,
+  ): Promise<CreateGroupResponse> {
+    if (!request || !request.name || request.name == '') {
+      throw new RpcException('Error on input name');
+    }
+
+    const right_auth = await this.checkAuthService.checkTokenApi(metadata);
+
+    if (!right_auth) {
+      throw new RpcException('Error unauthorized auth!');
+    }
+
+    return await this.groupService.create(request.name);
+  }
+
+  async findGroup(
+    request: GetGroupRequest,
+    metadata?: Metadata,
+  ): Promise<GetGroupResponse> {
+    if (!request || !request.groupId) {
+      throw new RpcException('Error on input group_id');
+    }
+
+    const right_auth = await this.checkAuthService.checkTokenApi(metadata);
+
+    if (!right_auth) {
+      throw new RpcException('Error unauthorized auth!');
+    }
+
+    const groupFounded = await this.groupService.get(request.groupId);
+
+    if (groupFounded == undefined) {
+      throw new RpcException('Error no group found');
+    }
+    return groupFounded;
+  }
 }
